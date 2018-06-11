44788bfc1f53a8666a8c9f8b0fa287ce64344d9a
孙冬 <sundong306@sohu.com>
2018-06-11 13:54:13 +0800

    Update install_zabbix_agent.sh

--- shell/Install/install_zabbix_agent.sh
+++ shell/Install/install_zabbix_agent.sh
@@ -8,6 +8,7 @@ dpkg -i zabbix-release_3.4-1+trusty_all.deb
 apt-get update
 apt-get install zabbix-agent
 sed  -i   '/#/d;/^$/d'  /etc/zabbix/zabbix_agentd.conf 
-sed  -i  's/ServerActive=127.0.0.1/ServerActive=123.207.237.245/g'  /etc/zabbix/zabbix_agentd.conf 
+sed  -i  's/Server=127.0.0.1/Server=10.0.4.11/g'  /etc/zabbix/zabbix_agentd.conf
+sed  -i  's/ServerActive=123.207.237.245/ServerActive=10.0.4.11/g'   /etc/zabbix/zabbix_agentd.conf
 sed  -i  "s/Hostname=Zabbix server/Hostname=${Name}/g" /etc/zabbix/zabbix_agentd.conf
 service  zabbix-agent restart
