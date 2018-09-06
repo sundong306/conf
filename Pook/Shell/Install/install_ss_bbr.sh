c220d57d40dcf2d387b45ff838a00ddad785fd12
孙冬 <sundong306@sohu.com>
2018-09-06 18:18:31 +0800

    Create install_ss_bbr.sh

---
+++ Pook/Shell/Install/install_ss_bbr.sh
@@ -0,0 +1,10 @@
+
+
+wget https://raw.githubusercontent.com/kuoruan/shell-scripts/master/ovz-bbr/ovz-bbr-installer.sh
+chmod +x ovz-bbr-installer.sh
+./ovz-bbr-installer.sh
+
+作者：不着调的小男生
+链接：https://www.jianshu.com/p/78bf7fa24e5e
+來源：简书
+简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
