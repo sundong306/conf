6fc3e4237b5c5501999de2d2f3037c7c4399f8ce
孙冬 <sundong306@sohu.com>
2019-03-22 18:46:30 +0800

    TSL 1.3
    TSL 1.3

--- Pook/Shell/Install/install_nginx_v1.sh
+++ Pook/Shell/Install/install_nginx_v1.sh
@@ -28,8 +28,8 @@ yum install  -y  pcre*  zlib*    gcc     openssl  openssl-devel  libxml*  libxsl
 ##安装nginx
 cd /opt ;  wget  https://www.openssl.org/source/openssl-1.1.1b.tar.gz  ; tar zxf  openssl-1.1.1b.tar.gz 
 cd  openssl-1.1.1b ;  ./config  ; make ;  make install ; openssl version
-cd /opt  ;  wget  http://nginx.org/download/nginx-1.12.0.tar.gz  ;tar  zxf  nginx-1.12.0.tar.gz 
-cd  nginx-1.12.0 
+cd /opt  ;  wget  https://nginx.org/download/nginx-1.15.9.tar.gz  ;tar  zxf  nginx-1.15.9.tar.gz 
+cd  nginx-1.15.9
 ./configure  --prefix=/usr/local/nginx --user=www --group=www --with-http_stub_status_module --with-http_ssl_module  --with-http_realip_module --with-http_geoip_module --with-stream  --with-stream_realip_module   --with-http_v2_module   --with-openssl=/opt/openssl-1.1.1b
 make  && make  install
 useradd  -M   -s /sbin/nologin  www
