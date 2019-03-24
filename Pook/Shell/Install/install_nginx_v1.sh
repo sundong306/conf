ba529cae4c8c28eb648adfd7673880f56dc9aea2
孙冬 <sundong306@sohu.com>
2019-03-24 11:32:12 +0800

    Update install_nginx_v1.sh

--- Pook/Shell/Install/install_nginx_v1.sh
+++ Pook/Shell/Install/install_nginx_v1.sh
@@ -7,6 +7,11 @@
 #
 ########################################
 
+####enable-tls1_3 是让 OpenSSL 支持 TLS 1.3 的关键选项；
+####而 enable-weak-ssl-ciphers 的作用是让 OpenSSL 继续支持 3DES 等不安全的 Cipher Suite，如果你打算继续支持 IE8，才需要加上这个选项。
+####除了 http_v2 和 http_ssl 这两个 HTTP/2 必备模块之外
+
+
 ## 非root用户无法执行  
 if [ $USER != root ]
 then
@@ -31,6 +36,7 @@ cd  openssl-1.1.1b ;  ./config  ; make ;  make install ; openssl version
 #cd  openssl-1.1.1b ;  openssl version
 cd /opt  ;  wget  https://nginx.org/download/nginx-1.15.9.tar.gz  ;tar  zxf  nginx-1.15.9.tar.gz 
 cd  nginx-1.15.9
+
 ./configure  --prefix=/usr/local/nginx --user=www --group=www  --with-http_ssl_module  --with-stream  --with-stream_realip_module   --with-http_v2_module   --with-openssl=/opt/openssl-1.1.1b    --with-openssl-opt='enable-tls1_3 enable-weak-ssl-ciphers'
 make  && make  install
 useradd  -M   -s /sbin/nologin  www
