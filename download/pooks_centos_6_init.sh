ad0a50be44891656cf16e6ee507ba113f299d4eb
孙冬 <sundong@pooks.cn>
2019-06-10 16:20:26 +0800

    swap
    swap

--- download/pooks_centos_6_init.sh
+++ download/pooks_centos_6_init.sh
@@ -37,23 +37,4 @@ free  -m
 fi
 
 
-##6.mysql 5.5  
-yum  install mysql-server
-#mysql  -h localhost  -uroot -p'Hawk@2018'
-
-
-##7. psql  9.5
-yum install postgresql
-mkdir -p /data/postgres/pgsql/data
-chown -R postgres.postgres /data/postgres/
-ls  /usr/lib/postgresql/9.5/bin/initdb
-su - postgres  -c  "/usr/lib/postgresql/9.5/bin/initdb  -D /data/postgres/pgsql/data"
-su - postgres  -c  "/usr/lib/postgresql/9.5/bin/pg_ctl  -D /data/postgres/pgsql/data -l /tmp/postrest.log start ""
-
-
-
-##8. redis 
-yum  install  redis-server
-# redis-cli   -h 10.72.12.234   -p 6379  -a '123456'
-
 
