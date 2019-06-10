72fa6fb2f31bf9e904879c8e02a7c630616c1d5d
孙冬 <sundong@pooks.cn>
2019-06-10 11:37:25 +0800

    1.html
    1.html

--- Pook/Shell/Install/install_nginx_v1.sh
+++ Pook/Shell/Install/install_nginx_v1.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 #####contact: sundong306@foxmail.com
 #####https://www.nginx.com/resources/admin-guide/installing-nginx-open-source/
-########################################
-#
-# OS:CentOS 7  
-#
-########################################
-
-####enable-tls1_3 是让 OpenSSL 支持 TLS 1.3 的关键选项；
-####而 enable-weak-ssl-ciphers 的作用是让 OpenSSL 继续支持 3DES 等不安全的 Cipher Suite，如果你打算继续支持 IE8，才需要加上这个选项。
-####除了 http_v2 和 http_ssl 这两个 HTTP/2 必备模块之外
-
-
 ## 非root用户无法执行  
 if [ $USER != root ]
 then
@@ -32,24 +21,24 @@ fi
 nginx_version=1.16.0
 nginx_download=/opt/download
 nginx_home=/data/app/nginx
-mkdir  -p  ${nginx_home}   ${nginx_download}
+mkdir  -p  ${nginx_home}  ${nginx_download}
 
 # yum
 yum install -y pcre-devel zlib-devel openssl-devel   gd-devel  GeoIP GeoIP-data GeoIP-devel   perl-ExtUtils-Embed  gcc-c++
 
 # openssl
 cd ${nginx_download}
-wget  https://www.openssl.org/source/openssl-1.1.1b.tar.gz  ; tar zxf  openssl-1.1.1b.tar.gz 
-cd  openssl-1.1.1b ;  ./config  ; make ;  make install ; /opt/openssl-1.1.1b/openssl version
+#wget  https://www.openssl.org/source/openssl-1.1.1b.tar.gz  ; tar zxf  openssl-1.1.1b.tar.gz 
+#cd  openssl-1.1.1b ;  ./config  ; make ;  make install ; ./openssl version
 
 # nginx
 cd ${nginx_download}
-wget http://nginx.org/download/nginx-${nginx_version}.tar.gz
-tar zxf  nginx-${nginx_version}.tar.gz
+#wget http://nginx.org/download/nginx-${nginx_version}.tar.gz
+#tar zxf  nginx-${nginx_version}.tar.gz
 cd ./nginx-${nginx_version}
 
 # make
-./configure --prefix=${nginx_home} --user=www --group=www  --with-http_ssl_module  --with-stream  --with-stream_realip_module   --with-http_v2_module   --with-openssl=/opt/openssl-1.1.1b
+./configure --prefix=${nginx_home} --user=www --group=www  --with-http_ssl_module  --with-stream  --with-stream_realip_module   --with-http_v2_module   --with-openssl=${nginx_download}/openssl-1.1.1b
 make & make install
 check_ok
 useradd  -M   -s /sbin/nologin  www
@@ -57,10 +46,8 @@ rm -rf  /usr/sbin/nginx
 ln -s ${nginx_home}/sbin/nginx /usr/sbin/nginx
 nginx  -V
 nginx  -t
-echo  "`date`"  >  ${nginx_home}/html/1.html
-nginx
-curl    http://localhost/1.html
 
+echo  "`date`"  >  ${nginx_home}/html/1.html
 # config
 #cp ${nginx_home}/conf/nginx.conf ${nginx_home}/conf/nginx.conf.bak
-#wget  https://raw.githubusercontent.com/sundong306/conf/master/download/pooks_http_nginx.conf  -O  ${nginx_home}/conf/nginx.conf 
+#wget  https://raw.githubusercontent.com/sundong306/conf/master/download/pooks_http_nginx.conf  -O  ${nginx_home}/conf/nginx.conf
