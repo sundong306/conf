47bdb025ba41e08b014552b1b4bf5bea893fdbb3
sundong306 <sundong@pooks.cn>
2019-06-10 14:29:24 +0800

    20190606

--- download/pooks_ubuntu_16.04_init.sh
+++ download/pooks_ubuntu_16.04_init.sh
@@ -17,7 +17,7 @@ source   /etc/profile
 apt install lrzsz lsof expect nethogs net-tools ntpdate sysstat dos2unix  tree wget file tcpdump  dstat   fping  iotop mtr rsync  python-pip   -y
 
 
-##4.修改ssh端口为 37878   允许root登录  修改sshd_config  
+##4.修改ssh端口为 37878/22   允许root登录  修改sshd_config  
 wget  https://raw.githubusercontent.com/sundong306/conf/master/download/pooks_ubuntu_16_04_aliyun_sshd_config -O   /etc/ssh/sshd_config
 
 
@@ -33,24 +33,6 @@ free  -m
 fi
 
 
-##6.mysql 5.5  
-apt-get install mysql-server
-#mysql  -h localhost  -uroot -p'Hawk@2018'
-
-
-##7. psql  9.5
-apt-get install postgresql
-mkdir -p /data/postgres/pgsql/data
-chown -R postgres.postgres /data/postgres/
-ls  /usr/lib/postgresql/9.5/bin/initdb
-su - postgres  -c  "/usr/lib/postgresql/9.5/bin/initdb  -D /data/postgres/pgsql/data"
-su - postgres  -c  "/usr/lib/postgresql/9.5/bin/pg_ctl  -D /data/postgres/pgsql/data -l /tmp/postrest.log start ""
-
-
-
-##8. redis 
-apt-get  install  redis-server
-# redis-cli   -h 10.72.12.234   -p 6379  -a '123456'
 
 
 
